cf2ba37f24d459fef333c479ed08781b4cda3954
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2023-01-24 15:15:59 +0100

    Finish challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import PatternDividerMobile from './assets/pattern-divider-mobile.svg';
+import PatternDividerDesktop from './assets/pattern-divider-desktop.svg';
+import DiceIcon from './assets/icon-dice.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
 
-  return <div className="text-3xl font-bold underline">{count}</div>;
+  useEffect(() => {
+    getNewAdvice();
+  }, []);
+
+  const getNewAdvice = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice', {
+        cache: 'no-cache',
+      });
+      const data = await response.json();
+      setData(data.slip);
+    } catch (error) {
+      setData({
+        id: 999,
+        advice: 'There was an error. Please try again later!',
+      });
+      console.error(error);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <main className="grid place-items-center bg-blue-900 h-full p-4">
+      <div
+        className="bg-blue-500 text-center px-6 py-8 pb-16 sm:px-10 sm:py-12 sm:pb-20 rounded-xl shadow-md relative max-w-[540px] data-[loading='true']:animate-pulse"
+        data-loading={isLoading}>
+        <h1 className="text-xs text-neon-green uppercase tracking-[.25em] mb-6">
+          Advice #{data?.id ?? '999'}
+        </h1>
+        <p className="text-2xl sm:text-[28px] text-light-cyan mb-6 md:mb-10">
+          {data?.advice ?? 'Loading...'}
+        </p>
+        <picture className="block">
+          <source media="(min-width: 500px)" srcSet={PatternDividerDesktop} />
+          <img className="min-w-full" src={PatternDividerMobile} alt="" />
+        </picture>
+        <button
+          className="bg-neon-green p-5 rounded-full absolute z-10 left-1/2 -bottom-8 -translate-x-1/2 shadow-lg hover:shadow-[0_0_30px_0px_rgba(82,255,168,1)] transition-shadow origin-center disabled:shadow-none"
+          disabled={isLoading}
+          onClick={getNewAdvice}>
+          <img src={DiceIcon} alt="" />
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'light-cyan': 'hsl(193, 38%, 86%)',
+      'neon-green': 'hsl(150, 100%, 66%)',
+      blue: {
+        100: 'hsl(217, 19%, 38%)',
+        500: 'hsl(217, 19%, 24%)',
+        900: 'hsl(218, 23%, 16%)',
+      },
+    },
   },
   plugins: [],
 };
